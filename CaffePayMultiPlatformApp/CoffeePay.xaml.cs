42f8074f5fa1da482f7113925ba6d812eeb9e098
h.bence <bence2118@gmail.com>
2023-03-12 13:23:45 +0100

    storage data and their correct display

--- CaffePayMultiPlatformApp/CoffeePay.xaml.cs
+++ CaffePayMultiPlatformApp/CoffeePay.xaml.cs
@@ -1,23 +1,24 @@
 using Microsoft.Maui.Storage;
 using System.Text.Json;
-using System.IO;
 
 namespace CaffePayMultiPlatformApp;
 
 public partial class CoffeePay : ContentPage
 {
-	string fileName = "CaffePayApp";
+	string fileName = "CaffePayApp.json";
 
-	Prices Prices = new Prices
-	{
-		Credits = 1000,
-		CoffeePrice = 150
-	};
+	Prices Prices;
+
+	int credits = 0;
+	int coffeePrice = 0;
 	
 	public CoffeePay()
 	{
 		InitializeComponent();
-	}
+        credits = Preferences.Get("Credits", 1000);
+        CreditsLb.Text = $"You have {credits}Ft for coffee.";
+        SemanticScreenReader.Announce(CreditsLb.Text);
+    }
 
     // Serialize and save
 	public void SerializeSave()
@@ -61,8 +62,16 @@ public partial class CoffeePay : ContentPage
 
 	private void PayOneCoffee(object sender, EventArgs e)
 	{
-		ReadandDeserialize();
-		if(Prices.Credits-Prices.CoffeePrice >= 0)
+		/*ReadandDeserialize();
+		if(Prices == null)
+		{
+            Prices = new Prices
+            {
+                Credits = 1000,
+                CoffeePrice = 150
+            };
+        }
+		if(Prices?.Credits-Prices?.CoffeePrice >= Prices?.CoffeePrice)
 		{
 			Prices.Credits -= Prices.CoffeePrice;
 
@@ -71,11 +80,50 @@ public partial class CoffeePay : ContentPage
 		else
 		{
 			CreditsLb.Text = $"Now you don't have enough credit to pay for a coffee.\nYou have {Prices.Credits}Ft";
-            SerializeSave();
+			Prices.Credits = 1000;
+            Prices.CoffeePrice = 150;
+            
         }
 
         SemanticScreenReader.Announce(CreditsLb.Text);
 		SerializeSave();
+		*/
+
+		if(!Preferences.ContainsKey("Credits") && !Preferences.ContainsKey("CoffeePrice"))
+		{
+            credits = 1000;
+            coffeePrice = 150;
+            Preferences.Set("Credits", credits);
+            Preferences.Set("CoffeePrice", coffeePrice);
+		}
+
+		credits = Preferences.Get("Credits", 0);
+		coffeePrice = Preferences.Get("CoffeePrice", 0);
+
+		if(credits == 0 && coffeePrice == 0)
+		{
+			credits = 1000;
+			coffeePrice = 150;
+			Preferences.Set("Credits", credits);
+			Preferences.Set("CoffeePrice", coffeePrice);
+		}
+
+		if(credits >= coffeePrice)
+		{
+			credits-= coffeePrice;
+            CreditsLb.Text = $"Now you have {credits}Ft for coffee.";
+            Preferences.Set("Credits", credits);
+        }
+		else
+		{
+            CreditsLb.Text = $"Now you don't have enough credit to pay for a coffee.\nYou have {credits}Ft and one coffee is {coffeePrice}Ft";
+            credits = 1000;
+            coffeePrice = 150;
+            Preferences.Set("Credits", credits);
+            Preferences.Set("CoffeePrice", coffeePrice);
+        }
+
+        SemanticScreenReader.Announce(CreditsLb.Text);
 
     }
 }
